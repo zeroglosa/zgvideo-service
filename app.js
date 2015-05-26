9fbfbb5c8c3fcf5dc6388130b17d1be70bd90350
Halisson <halisson.fibonacci@solucoesageis.com.br>
2015-05-26 18:00:41 -0300

    mounting server on '/'

--- app.js
+++ app.js
@@ -24,11 +24,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
 
-vidStreamer = vidStreamer.settings(config);
-
-app.use('/videos', vidStreamer);
+app.use('/', vidStreamer.settings(config));
 
 //catch 404 and forward to error handler
 app.use(function (req, res, next) {
