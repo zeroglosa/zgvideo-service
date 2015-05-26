8a88cbbe9451f2cc710d20ffce9322c2beb30812
Halisson <halisson.fibonacci@solucoesageis.com.br>
2015-05-26 17:47:30 -0300

    fixing key

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ var settings = {
 	random: valueOrDefault(ENV["ZGVIDEO_RANDOM"], false),
 	rootFolder: valueOrDefault(ENV["ZGVIDEO_ROOT_FOLDER"], 'videos'),
 	rootPath: valueOrDefault(ENV["ZGVIDEO_ROOT_PATH"], ''),
-	server: valueOrDefault(ENV["SERVER"], 'ZGVideoService'),
+	server: valueOrDefault(ENV["ZGVIDEO_SERVER"], 'ZGVideoService'),
 	maxAge: valueOrDefault(ENV["ZGVIDEO_MAX_AGE"], 3600),
 	throttle: valueOrDefault(ENV["ZGVIDEO_THROTTLE"], false)
 };
