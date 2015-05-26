f79843c128c199602f558f20849216b38f3dc0d1
Halisson <halisson.fibonacci@solucoesageis.com.br>
2015-05-26 18:00:28 -0300

    fixing keys

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ var ENV = process.env;
 
 var settings = {
 	mode: valueOrDefault(ENV["NODE_ENV"], 'development'),
-	forceDownload: valueOrDefault(ENV["ZGVIDEO_FORCE_DOWNLOAD"]),
+	forceDownload: valueOrDefault(ENV["ZGVIDEO_FORCE_DOWNLOAD"], false),
 	random: valueOrDefault(ENV["ZGVIDEO_RANDOM"], false),
 	rootFolder: valueOrDefault(ENV["ZGVIDEO_ROOT_FOLDER"], 'videos'),
 	rootPath: valueOrDefault(ENV["ZGVIDEO_ROOT_PATH"], ''),
